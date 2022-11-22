c816a75eb08722275a8c86784bd12b6cc2b0f67f
MinJ33 <erica990604@gmail.com>
2022-11-22 22:58:25 +0900

    feat: Create a throttled function

--- js/main.js
+++ js/main.js
@@ -14,3 +14,16 @@ searchInputEl.addEventListener('blur', function() {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+// _.throttle(func, time)
+// create a throttled function that can only call the func parameter maximally once per every 0.3sec
+window.addEventListener('scroll', _.throttle(function() {
+  console.log('scroll!');
+  if (window.scrollY > 500){
+    badgeEl.style.display = 'none';
+  } else {
+    badgeEl.style.display = 'block';
+  }
+  // 0.3 sec
+}, 300));
