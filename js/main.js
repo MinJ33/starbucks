a9227caac61258e01be5fbd02fd8efd752cd9920
MinJ33 <erica990604@gmail.com>
2022-12-05 02:13:43 +0900

    feat: Add swiper arrows

--- js/main.js
+++ js/main.js
@@ -74,7 +74,11 @@ new Swiper('.awards .swiper-container', {
   autoplay: true,
   loop: true,
   spaceBetween: 30,
-  slidesPerView: 5
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
 });
 
 // This section will be show || hide
